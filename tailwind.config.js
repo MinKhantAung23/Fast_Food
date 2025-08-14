caa2907555ae57dd802da953afb5b7ac0f65adcb
Min Khant Aung <minkhantaung.dev.lmp@gmail.com>
2025-08-14 13:06:52 +0630

    set up base filte and folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FE8C00",
+        white: {
+          default: "#FFFFFF",
+          100: "#fafafa",
+          100: "#fe8c00",
+        },
+        gray: {
+          100: "#878787",
+          200: "#878787",
+        },
+        dark: {
+          100: "#181C2E",
+        },
+        error: "#f14141",
+        success: "#2f9b65",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand-Regular", "sans-serif"],
+        "quicksand-bold": ["Quicksand-Bold", "sans-serif"],
+        "quicksand-semibold": ["Quicksand-SemiBold", "sans-serif"],
+        "quicksand-light": ["Quicksand-Light", "sans-serif"],
+        "quicksand-medium": ["Quicksand-Medium", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
